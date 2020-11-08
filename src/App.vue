311b1454888a0175c6a01b854b56c15db442b166
AshimbekovNurdaulet <nurdaulet.ashimbekov@gmail.com>
2020-11-08 20:18:34 +0600

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/example">Example</router-link>
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/example">Example</router-link>-->
+
+      <header>
+        <div class="navbar">
+          <div class="container">
+            <div class="navbar-content">
+              <div class="logo">VUE-CLI</div>
+              <ul class="navbar-list">
+                <li class="navbar-item">
+                  <div class="navbar-link">
+                    <a class="navbar-link">Home</a>
+                  </div>
+                </li>
+                <li class="navbar-item">
+                  <div class="navbar-link">
+                    <a class="navbar-link">Example</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
-    <router-view/>
+<!--    <router-view/>-->
+
+  <router-view></router-view>
+
   </div>
 </template>
 
