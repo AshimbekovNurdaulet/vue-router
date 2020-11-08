14856902188cf6b80b0be7145ea623ef56b8c09a
AshimbekovNurdaulet <nurdaulet.ashimbekov@gmail.com>
2020-11-08 19:48:53 +0600

    installing vue-router

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// Pages
+import Home from '@/pages/Home'
+import Example from '@/pages/Example'
+
+// Routering
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: '/home',
+            component: Home
+        },
+        {
+            path: '/examplen',
+            name: '/example',
+            component: Example
+        },
+    ]
+})
